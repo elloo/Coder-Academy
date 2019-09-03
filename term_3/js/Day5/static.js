e7e263bc8aedb7561a66865ab2bcff33364b9190
Ewe Lin Loo <ewelinloo@gmail.com>
2019-09-03 13:06:17 +1000

    Remove this.props

--- term_3/js/Day5/static.js
+++ term_3/js/Day5/static.js
@@ -1,17 +1,24 @@
 // var count = 0;
 class Dog {
+    static setCount () {
+        Dog.count = 0;
+    }
     constructor(name) {
         this.name = name;
         Dog.count++;
     }
+
     static dogCount() {
         return `the dog count is ${Dog.count}`;
     }
 }
-// initialze the count variable for the Dog class
-Dog.count = 0;
+// initialze the count variabel for the Dog class
+// Dog.count = 0;
+
+// alternate syntax
+Dog.setCount();
 
 var dog1 = new Dog("Tommy");
 var dog2 = new Dog("Rex");
 
-console.log(Dog.dogCount())
+console.log(Dog.dogCount());
